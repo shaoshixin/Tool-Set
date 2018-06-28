6caf16155e9eb1e225a8711578b8ce477def9ef9
SCZ_ZJZHD <shaoshixin842@live.com>
2018-06-28 22:46:47 +0800

    628：基本框架搭好

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-import OpenFile
-import BuildDBConfig
+import OpenFileFrame
+import BuildDBConfigFrame
+import TestUrlFrame
 
 
 class CreatMain:
@@ -9,29 +10,39 @@ class CreatMain:
         self.main_menu = tk.Menu(self.root)
         # Frame页面创建
         self.main_frm = tk.Frame(self.root)
-        self.openfile_frm = OpenFile.OpenFile(self.root)
-        self.builddbconfig_frm = BuildDBConfig.XML(self.root)
+        self.openfile_frm = OpenFileFrame.OpenFile(self.root)
+        self.buildbconfig_frm = BuildDBConfigFrame.XML(self.root)
+        self.testurl_frm = TestUrlFrame.TestUrlFrame(self.root)
 
         self.creatMenu()
         self.creatMainFrm()
         # 默认显示主页面Frame
-        self.main_frm.pack()
+        self.main_frm.grid()
 
     # 主页切换菜单显示
 
     def creatMenu(self):
+        # filemenu按钮下拉功能
         self.file_menu = tk.Menu(self.main_menu)
         self.file_menu.add_command(
             label='主页', command=lambda: self.showFrm(self.main_frm))
         self.file_menu.add_command(
             label='打开文件', command=lambda: self.showFrm(self.openfile_frm))
 
+        # xmlmenu下拉功能按钮
         self.xml_menu = tk.Menu(self.main_menu)
         self.xml_menu.add_command(
-            label="BuildDBConfig", command=lambda: self.showFrm(self.builddbconfig_frm))
+            label="BuildDBConfig", command=lambda: self.showFrm(self.buildbconfig_frm))
 
+        # 接口测试
+        self.spilder_menu = tk.Menu(self.main_menu)
+        self.spilder_menu.add_command(
+            label="接口测试", command=lambda: self.showFrm(self.testurl_frm))
+
+        # 根menu绑定至menu上
         self.main_menu.add_cascade(label="文件", menu=self.file_menu)
         self.main_menu.add_cascade(label="XML文件生成", menu=self.xml_menu)
+        self.main_menu.add_cascade(label="接口测试", menu=self.spilder_menu)
         self.root.config(menu=self.main_menu)
 
     # 主页面Frame视图
@@ -42,30 +53,30 @@ class CreatMain:
     # Frame切换函数
     def showFrm(self, frame):
         # 所有生成的Frame页面
-        self.frames = [self.main_frm,
-                       self.openfile_frm, self.builddbconfig_frm]
+        self.frames = [self.main_frm, self.testurl_frm,
+                       self.openfile_frm, self.buildbconfig_frm]
         self.frames.remove(frame)
         for f in self.frames:
-            f.pack_forget()
-        frame.pack()
+            f.grid_forget()
+        frame.grid()
 
 
 '''
 废弃的frame显示切换方法
     def _mainFrm(self):
-        self.main_frm.pack()
-        self.openfile_frm.pack_forget()
-        self.builddbconfig_frm.pack_forget()
+        self.main_frm.grid()
+        self.openfile_frm.grid_forget()
+        self.builddbconfig_frm.grid_forget()
 
     def _openFile(self):
-        self.main_frm.pack_forget()
-        self.openfile_frm.pack()
-        self.builddbconfig_frm.pack_forget()
+        self.main_frm.grid_forget()
+        self.openfile_frm.grid()
+        self.builddbconfig_frm.grid_forget()
 
     def _builddbconfigFrm(self):
-        self.main_frm.pack_forget()
-        self.openfile_frm.pack_forget()
-        self.builddbconfig_frm.pack()
+        self.main_frm.grid_forget()
+        self.openfile_frm.grid_forget()
+        self.builddbconfig_frm.grid()
 '''
 
 if __name__ == '__main__':
